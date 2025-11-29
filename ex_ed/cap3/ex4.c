c7afa149a140bb0b4867055f1125a35df4a809ed
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2025-11-29 17:01:54 -0300

    questao 4

---
+++ ex_ed/cap3/ex4.c
@@ -0,0 +1,4 @@
+/*Implemente uma função que permita a avaliação de polinômios. 
+Cada polinômio é definido por um vetor que contém seus coeficientes. Por exemplo,
+ o polinômio de grau 2, 
+double avalia(double* poli, int grau, double x);*/
